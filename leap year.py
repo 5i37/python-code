d3020e3d32edc50ea5cbc4626ce656ded36cf936
5i37 <kwkr2013@gmail.com>
2023-07-26 13:50:49 +0400

    know the leap year

---
+++ leap year.py
@@ -0,0 +1,13 @@
+year = int(input("enter the year your want : "))
+
+x  =  0
+ky = 0
+while x <= year :
+    x+=0
+    ky = ky + 4 
+    if year == ky :
+        print ("this year is Leap year")
+        break
+    else :
+        print ("this year is Leap year")
+        break

e080b493adfc3c06dfcd848a9e820c5b13d48d07
Khalid waleed alrashdi <138435240+5i37@users.noreply.github.com>
2023-07-27 13:09:09 +0400

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,38 @@
+
+n = 6
+for x in range(1,6):
+    print("")
+    n-=1
+    
+    for i in range(1,n):
+        
+        print(i,end="")
+        
+        
+        
+
+
+
+
+
+
+
+"""   
+print("")
+for i in range(1,3):
+    print(i," ",end="")
+print("")
+
+for i in range(1,2):
+    print(i," ",end="")
+        """
+
+
+for x in range(1,9) :
+    print("*")
+
+
+
+
+
+
---
+++ loop.py
@@ -0,0 +1,22 @@
+y=0
+for x in range(9,1,-1) :
+    
+    
+    for y in range (1,9):
+        print("*"*y,end="")
+        print("")
+        y+=1
+    break
+
+n = 9
+for x in range(1,9):
+    
+    
+    for i in range(1,n):
+        
+        print("*",end="")
+        
+      
+    print("")
+    n-=1
+    

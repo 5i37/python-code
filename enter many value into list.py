9a609ec7b67072a521065863cc943f198d20eb7f
Khalid waleed alrashdi <138435240+5i37@users.noreply.github.com>
2023-08-01 10:49:52 +0400

    enter many value into list

---
+++ enter many value into list.py
@@ -0,0 +1,13 @@
+
+
+value = []
+
+print("enter values, Q to quit")
+userinput = input()
+while userinput.upper() !="Q":
+    value.append(float(userinput))
+    userinput = input("")
+    
+    
+    
+print(value)
